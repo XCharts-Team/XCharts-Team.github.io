fe9eb020465cf9f55ac7ca13f073d2a717075389
monitor1394 <monitor1394@gmail.com>
2023-06-08 08:29:48 +0800

    3.7.0

--- website/sync_docs_all.sh
+++ website/sync_docs_all.sh
@@ -9,5 +9,5 @@ XCHARTS_DEMO=$XCHARTS_WEBSITE/../../xcharts-demo.github.io
 cd $XCHARTS_WEBSITE
 yarn install
 
-python3 utils/sync_version.py $XCHARTS_MAIN $XCHARTS_WEBSITE $XCHARTS_DEMO v3.6.0
+python3 utils/sync_version.py $XCHARTS_MAIN $XCHARTS_WEBSITE $XCHARTS_DEMO v3.7.0
 python3 utils/sync_version.py $XCHARTS_MAIN $XCHARTS_WEBSITE $XCHARTS_DEMO master
